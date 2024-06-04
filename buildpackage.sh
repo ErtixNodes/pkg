00850d56a326efd77cdc5eb9f37ed60023ed728b
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-06-04 17:13:10 +0200

    Remove verbose shit

--- buildpackage.sh
+++ buildpackage.sh
@@ -6,14 +6,10 @@ if [ "$1" == "./repo" ] || [ "$1" == "./repo/x86_64" ]; then
     echo "Ignoring $1...."
 else
     echo "Processing directory: $1"
-
-    cd "$1"
-    echo "$(pwd)"
     abuild -P $dir build
 
     abuild-sign $(pwd)/../
-    find $(pwd)/../../
-    abuild -P $dir -v index
+    abuild -P $dir index
 
     echo "Built $1!"
 fi
