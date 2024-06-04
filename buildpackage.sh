d1787f8fedb06e157c853cef906d24ef4c16acf5
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-06-04 17:19:03 +0200

    Add key

--- buildpackage.sh
+++ buildpackage.sh
@@ -8,7 +8,7 @@ else
     echo "Processing directory: $1"
     abuild -P $dir build
 
-    abuild-sign $(pwd)/../
+    abuild-sign -k ~/.abuild/bttm-6650b009.rsa $(pwd)/../
     abuild -P $dir index
 
     echo "Built $1!"
