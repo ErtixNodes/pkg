e33d7a6e61b78d7755d4e04856e7f64ae49e17a0
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-06-16 12:22:51 +0200

    Fix :D

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-wget -P /etc/apk/keys https://ertixnodes.github.io/pkg/info@bastothemax.nl-666178c1.rsa.pub
+apk add curl
+curl -O /etc/apk/keys https://ertixnodes.github.io/pkg/info@bastothemax.nl-666178c1.rsa.pub
 echo "https://ertixnodes.github.io/pkg/repo" >> /etc/apk/repositories
 apk update
